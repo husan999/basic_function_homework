02b46bab19145aa8f5b4f9b96ed7dcfe82ae1b5c
husan999 <husanshunqorov07@gmail.com>
2023-02-21 08:10:56 -0700

    good

--- basic_func05.py
+++ basic_func05.py
@@ -1,8 +1,6 @@
 # Create a function called main.
 
 # Return the value float type.
-def main(a):
-    return a
-x=float(main(4))
-print(x)
-print(type(x))
+def main():
+    return 23.1
+print(main())
