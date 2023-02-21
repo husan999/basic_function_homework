561860708b8eeab96455df4e14724c75cd81ba94
husan999 <husanshunqorov07@gmail.com>
2023-02-21 08:11:55 -0700

    good

--- basic_func06.py
+++ basic_func06.py
@@ -1,8 +1,6 @@
 # Create a function called main.
 
 # Return the value str type.
-def main(a):
-    return a
-x=str(main(2.4))
-print(x)
-print(type(x))
+def main():
+    return "jahon"
+print(main())
